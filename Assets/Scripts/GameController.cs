0542984b0f07685d088e1286cd70fbe42a951553
Sciuzza <juza9164@gmail.com>
2016-11-07 20:08:25 +0100

    Fixed Some Code Bugs Created the Score System Fixed the Scene Switching

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,33 +6,83 @@ using UnityEngine.Events;
 public class GameController : MonoBehaviour {
 
 
-	public int[] VictimClues = new int[4];
-	public int[] DetectClues = new int[4];
+	public const int[] VictimClues = new int[4];
+	public const int[] DetectClues = new int[4];
 
 	private int currentSceneIndex;
 
 	private bool isVictimStory = false;
+	private bool isOnGame = false;
+	private bool isMaxScore = false;
 
 	void Start () {
-		
+
 		Scene currentScene = SceneManager.GetActiveScene ();
 		currentSceneIndex = currentScene.buildIndex;
 
-		ExitBeha cmTempLink = GameObject.FindGameObjectWithTag("Exit").GetComponent<ExitBeha>();
-		cmTempLink.levelFinished.AddListener(LevelCompleteManager);
+		SettingVariables (currentSceneIndex);
+
+		if (currentScene.buildIndex >= 6) {
+			
+			ExitBeha cmTempLink = GameObject.FindGameObjectWithTag ("LevelCom").GetComponent<ExitBeha> ();
+
+			if (cmTempLink == null)
+				Debug.LogWarning ("Missing Exit");
+
+			cmTempLink.levelFinished.AddListener (LevelCompleteManager);
+		} 
+		else if (currentScene.buildIndex == 3) {
+
+
+			if (isMaxScore)
+				Debug.Log("Max Score");
+			else
+				Debug.Log("Normal Score");
+		}
 	}
 
 	private void LevelCompleteManager(int sceneToGo){
 
-		//Prende il gestore dell'inventario e legge il numero degli indizi che sono stati trovati
-		//Controlla se è la storia della vittima o del detective
-		//Controlla quanti indizi massimi dovrebbe avere la scena corrente
-		//A seconda della percentuale raggiunta fa partire un background piuttosto che un altro nella
-		//scena di caricamento
-		/*
-		Inventory invTempLink = GameObject.FindGameObjectWithTag ("Inventory");
-		int currentScore = invTempLink.array.length ();
-        */
+
+
+		Inventory invTempLink = GameObject.FindGameObjectWithTag ("Inventory").GetComponent<Inventory>();
+		int currentScore = invTempLink.itemContainer.Count;
+        
+
+		int maxScore;
+
+		if (isVictimStory) {
+			maxScore = VictimClues [sceneToGo - 6];
+		} 
+		else {
+			maxScore = DetectClues [sceneToGo - 10];
+		}
+
+		if (currentScore == maxScore)
+			isMaxScore = true;
+		else
+			isMaxScore = false;
+
+		SceneManager.LoadScene (sceneToGo);
+
 	}
 
+	private void SettingVariables(int buildIndex){
+
+		if (buildIndex >= 6) {
+
+			if (buildIndex <= 9)
+				isVictimStory = true;
+			else
+				isVictimStory = false;
+
+			isOnGame = true;
+			isMaxScore = false;
+
+
+		}
+		else
+			isOnGame = false;
+			
+	}
 }
