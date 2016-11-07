66c0956bb7b2186ed4ad4c7f07b269034ac07ae2
Game-PC\Game <fabrizio.caggianelli@gmail.com>
2016-11-07 18:50:10 +0100

    gestione invetario e tooltip

--- Assets/Scripts/ClickableHandler.cs
+++ Assets/Scripts/ClickableHandler.cs
@@ -5,15 +5,41 @@ using System.Collections;
 public class ClickableHandler : MonoBehaviour {
 
     Inventory refInv;
-    
+    public bool isClicked = false;
+    public SpriteRenderer[] outline;
+    public UIHandler refUIHandler;
+    public string textTooltip;
+
     
     void Start()
     {
-        refInv = GetComponent<Inventory>();
+        refUIHandler = FindObjectOfType<UIHandler>();
+        outline = GetComponentsInChildren<SpriteRenderer>();
+        refInv = FindObjectOfType<Inventory>();
     }
-	void OnMouseUp()
+    void OnMouseOver()
+    {       
+        outline[1].color = Color.white;
+    }
+    void OnMouseExit()
     {
-        refInv.SetInventory(this.gameObject);
+        outline[1].color = Color.clear;
+    }
+
+    void OnMouseUp()
+    {
+        if (!isClicked)
+        {
+            refUIHandler.CluePanelActivator(textTooltip);
+            isClicked = true;
+        }
+        else if (isClicked)
+        {
+            refUIHandler.CluePanelDeactivator(); 
+            refInv.SetInventory(this.gameObject);
+            isClicked = false;
+        }       
+        
     }
     
 }
---
+++ Assets/Scripts/UIHandler.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIHandler : MonoBehaviour {
+
+    public GameObject panelClue;
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void CluePanelActivator(string text)
+    {
+        panelClue.SetActive(true);
+        panelClue.GetComponentInChildren<Text>().text = text;
+    }
+    public void CluePanelDeactivator()
+    {
+        panelClue.SetActive(false);
+    }
+}
