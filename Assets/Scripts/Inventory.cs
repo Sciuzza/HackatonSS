eeac59c8735998e3afa57ead7d1b8dd841a63911
Game-PC\Game <fabrizio.caggianelli@gmail.com>
2016-11-07 17:13:25 +0100

    Inventory

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -1,31 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Events;
 
-public class Prova_fabri : MonoBehaviour {
-    public GameObject prefab;
+public class Inventory : MonoBehaviour {
+    public UnityEvent takeEvent;
+    GameObject prefab;
     public int n = 5;
     public GameObject[] container;
 	// Use this for initialization
 	void Start () {
-
+        prefab = Resources.Load("SlotInvetario") as GameObject;
         container = new GameObject[n];
         for (int i = 0; i < n; i++)
         {
             GameObject prefabGO = Instantiate(prefab);
-            container[i] = prefabGO;
-            prefabGO.transform.position = new Vector3(-2    +i, -4.5f, 0);
+            //container[i] = prefabGO;
+            prefabGO.transform.position = new Vector3(-2+i, -4.5f, 0);
             
             
         }
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
 
     void OnMouseUp()
     {
         Debug.Log("Click");
+    }
+
+    public void SetInventory(GameObject item)
+    {
+        
     }
 }
