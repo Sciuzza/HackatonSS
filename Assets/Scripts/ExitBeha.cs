d14edf7c19c63bd3605f8d46bcf0b8c303993529
Sciuzza <juza9164@gmail.com>
2016-11-07 18:51:00 +0100

    Created all the scenes in game Created Game Controller that will take the Clues Score in charge

--- Assets/Scripts/ExitBeha.cs
+++ Assets/Scripts/ExitBeha.cs
@@ -1,12 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using UnityEngine.Events;
 
 public class ExitBeha : MonoBehaviour {
 
 	public int SceneToGo;
 
+
+
+	[System.Serializable]
+	public class MyIntEvent : UnityEvent<int>
+	{
+	}
+
+	public MyIntEvent levelFinished;
+
+
+
 	void OnMouseDown() {
-		SceneManager.LoadScene (SceneToGo);
+		levelFinished.Invoke (SceneToGo);
 	}
 }
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.Events;
+
+public class GameController : MonoBehaviour {
+
+
+	public int[] VictimClues = new int[4];
+	public int[] DetectClues = new int[4];
+
+	private int currentSceneIndex;
+
+	private bool isVictimStory = false;
+
+	void Start () {
+		
+		Scene currentScene = SceneManager.GetActiveScene ();
+		currentSceneIndex = currentScene.buildIndex;
+
+		ExitBeha cmTempLink = GameObject.FindGameObjectWithTag("Exit");
+		cmTempLink.levelFinished.AddListener(LevelCompleteManager);
+	}
+
+	private void LevelCompleteManager(int sceneToGo){
+
+		//Prende il gestore dell'inventario e legge il numero degli indizi che sono stati trovati
+		//Controlla se è la storia della vittima o del detective
+		//Controlla quanti indizi massimi dovrebbe avere la scena corrente
+		//A seconda della percentuale raggiunta fa partire un background piuttosto che un altro nella
+		//scena di caricamento
+		/*
+		Inventory invTempLink = GameObject.FindGameObjectWithTag ("Inventory");
+		int currentScore = invTempLink.array.length ();
+        */
+	}
+
+}
