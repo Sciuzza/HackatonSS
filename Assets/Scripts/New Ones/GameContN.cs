4d20c4af82fcc45ea3e682a260dfbac2a80455cc
Cristiano <Juza9164@gmail.com>
2016-12-07 16:51:37 +0100

    Finale Phase 0.1

---
+++ Assets/Scripts/New Ones/GameContN.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.Events;
+using System.Collections.Generic;
+
+#region Event Classes
+[System.Serializable]
+public class event_int_bool_int : UnityEvent<int, bool, int>
+{
+}
+
+[System.Serializable]
+public class event_int : UnityEvent<int>
+{
+}
+#endregion
+
+
+public class GameContN : MonoBehaviour {
+
+
+}
