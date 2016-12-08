99002dbcf7726504f9df30246a5e679663fd3ee2
MSI\Kla <boongie123@gmail.com>
2016-12-08 13:51:32 +0100

    psd importer, texts importer, missing outline, project importer/scene creator 30%

---
+++ Assets/Scripts/Tool/SceneCreator.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.IO;
+
+public class SceneCreator : MonoBehaviour
+{
+    public int currentScene = 0;
+    List<int> projectValues;
+    GameObject logicPrefab, uiPrefab;
+
+	void Awake ()
+    {
+        string path = Application.persistentDataPath + "/scene.txt";
+        StreamReader sr = File.OpenText(path);
+        projectValues = new List<int>();
+        int s;
+        while (sr.ReadLine() != null)
+        {
+            s = int.Parse(sr.ReadLine());
+            projectValues.Add(s);
+        }
+        sr.Close();
+
+        if (currentScene<projectValues[0])
+        {
+            SceneManager.CreateScene("Scene" + currentScene + 1);
+            Instantiate(logicPrefab);
+            Instantiate(Resources.Load(currentScene + ".psd"));
+            Instantiate(uiPrefab);
+
+        }
+        else
+        {
+            //goto finale;
+        }
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
