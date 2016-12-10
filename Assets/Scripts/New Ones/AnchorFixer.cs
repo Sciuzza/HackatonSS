84ed7ef32e462fe76a62a172eb529c52233701e5
Cristiano <Juza9164@gmail.com>
2016-12-10 15:05:42 +0100

    AnchorFixer to fix anchors

---
+++ Assets/Scripts/New Ones/AnchorFixer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnchorFixer : MonoBehaviour
+{
+
+ 
+    void Start()
+    {
+
+        RectTransform rtTempLink = this.GetComponent<RectTransform>();
+
+        rtTempLink.pivot = new Vector2(0, 0);
+
+        float rectMaxX = Screen.width + rtTempLink.offsetMax.x;
+        float rectMaxY = Screen.height + rtTempLink.offsetMax.y;
+
+        Vector2 anchorMinPos = new Vector2((rtTempLink.position.x) / Screen.width, (rtTempLink.position.y) / Screen.height);
+        Vector2 anchorMaxPos = new Vector2(rectMaxX / Screen.width, rectMaxY / Screen.height);
+        rtTempLink.offsetMin = new Vector2(0, 0);
+        rtTempLink.offsetMax = new Vector2(0, 0);
+        rtTempLink.anchorMin = anchorMinPos;
+        rtTempLink.anchorMax = anchorMaxPos;
+
+    }
+
+
+}
