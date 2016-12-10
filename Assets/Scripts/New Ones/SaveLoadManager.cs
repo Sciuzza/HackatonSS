10bca21268c991e9b4286a0db4ffb370ea8b8bf0
MSI\Kla <boongie123@gmail.com>
2016-12-10 19:36:59 +0100

    no message

--- Assets/Scripts/New Ones/SaveLoadManager.cs
+++ Assets/Scripts/New Ones/SaveLoadManager.cs
@@ -6,10 +6,10 @@ using System.IO;
 public class SaveLoadManager : MonoBehaviour
 {
 
-	public string path = Application.persistentDataPath;
+    public string path;
 
-	#region Taking References and Linking Events
-	void Awake()
+    #region Taking References and Linking Events
+    void Awake()
 	{
 		GameContN gcTempLink = this.gameObject.GetComponent<GameContN>();
 
@@ -17,35 +17,43 @@ public class SaveLoadManager : MonoBehaviour
 		gcTempLink.saveDataRequest.AddListener(SavingDataOnCsv);
 
 	}
-	#endregion
+    #endregion
 
-	private void LoadingDataFromCsv()
-	{
-		string[] tempPaths = new string[];
-		ReadCsv(path + "maps.csv");
-
+    private void LoadingDataFromCsv()
+    {
+        path = Application.persistentDataPath;
+        List<string[]> nextStepPath = new List<string[]>();
+        //string[] tempPaths = new string[];
+        ReadCsv(path + "maps.csv", out nextStepPath);
+        /*
+        sensibleMapData newCity;
+        newCity.mapName = nextStepPath[0][0];
+            GameContN.playerDatasStatic.mapData.Add(new sensibleMapData());
         // fede qui ci sei tu, i dati che la logica utilizzerà sono nella variabile GameContN.playerdataStati, salva tutto li dentro
-
+        */
         GameContN.Debugging("Data Loaded");
 
       
     }
 
-	private void ReadCsv(string _fileName, out List<string> readOutPut) 
-	{
-		List<string> readOutPut = new List<string>();
-		int counter = 0;
-		StreamReader sr = File.OpenText(path + _fileName);
-		string s;
-		while ((s = sr.ReadLine()) != null)
-		{
-			if (counter == 0)
-			{
-				counter++;
-				continue;
-			}
-			readOutPut.Add(sr.ReadLine());
-	} 
+    private void ReadCsv(string _fileName, out List<string[]> readOutPut)
+    {
+        char[] separator = { ',' };
+        readOutPut = new List<string[]>();
+        int counter = 0;
+        StreamReader sr = File.OpenText(path + _fileName);
+        string s;
+        while ((s = sr.ReadLine()) != null)
+        {
+            if (counter == 0)
+            {
+                counter++;
+                continue;
+            }
+            readOutPut.Add(sr.ReadLine().Split(separator));
+
+        }
+    }
 
     private void SavingDataOnCsv()
     {
