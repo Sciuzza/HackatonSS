abf5fbb96dca61c98639255c4153814ed2f7d91a
Cristiano <Juza9164@gmail.com>
2016-12-10 16:52:26 +0100

    AnchorFixer Wip

--- Assets/Scripts/New Ones/AnchorFixer.cs
+++ Assets/Scripts/New Ones/AnchorFixer.cs
@@ -10,12 +10,29 @@ public class AnchorFixer : MonoBehaviour
     void Start()
     {
 
+        
+
         RectTransform rtTempLink = this.GetComponent<RectTransform>();
+
+     
 
         rtTempLink.pivot = new Vector2(0, 0);
 
-        float rectMaxX = Screen.width + rtTempLink.offsetMax.x;
-        float rectMaxY = Screen.height + rtTempLink.offsetMax.y;
+        //rtTempLink.anchorMin = new Vector2(0, 0);
+        //rtTempLink.anchorMax = new Vector2(1, 1);
+        rtTempLink.
+
+        Debug.Log(((rtTempLink.anchorMax.x * 1000) / 1000));
+        Debug.Log(rtTempLink.anchorMin);
+        Debug.Log(Screen.width);
+        Debug.Log(Screen.height);
+        Debug.Log(rtTempLink.offsetMax);
+
+        float rectMaxX = Screen.width - ((Mathf.Abs((rtTempLink.offsetMax.x / (rtTempLink.anchorMax.x * 1000) / 1000 )) * 1000) / 1000);
+        float rectMaxY = Screen.height - Mathf.Abs(rtTempLink.offsetMax.y / rtTempLink.anchorMax.y );
+
+        Debug.Log(rectMaxX);
+
 
         Vector2 anchorMinPos = new Vector2((rtTempLink.position.x) / Screen.width, (rtTempLink.position.y) / Screen.height);
         Vector2 anchorMaxPos = new Vector2(rectMaxX / Screen.width, rectMaxY / Screen.height);
