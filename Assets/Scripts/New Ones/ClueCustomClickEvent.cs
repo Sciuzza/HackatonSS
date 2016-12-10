45600d5d2ea6b667e537c70469621feeb6d3fc86
Zambon Federico <zambon.fe@gmail.com>
2016-12-10 15:59:40 +0100

    no message

---
+++ Assets/Scripts/New Ones/ClueCustomClickEvent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+
+public class ClueCustomClickEvent : MonoBehaviour, IPointerClickHandler
+{
+	public string clueName;
+
+    public event_string customClick;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        customClick.Invoke(clueName);
+    }
+}
