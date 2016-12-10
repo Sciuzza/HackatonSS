db564e7ac0bb198ac77a2df4d9a022f60944b8e1
Zambon Federico <zambon.fe@gmail.com>
2016-12-10 18:46:53 +0100

    beginning csv importer

--- Assets/Scripts/New Ones/SaveLoadManager.cs
+++ Assets/Scripts/New Ones/SaveLoadManager.cs
@@ -1,23 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
-public class SaveLoadManager : MonoBehaviour {
+public class SaveLoadManager : MonoBehaviour
+{
 
+	public string path = Application.persistentDataPath;
 
-    #region Taking References and Linking Events
-    void Awake()
-    {
-        GameContN gcTempLink = this.gameObject.GetComponent<GameContN>();
+	#region Taking References and Linking Events
+	void Awake()
+	{
+		GameContN gcTempLink = this.gameObject.GetComponent<GameContN>();
 
-        gcTempLink.loadDataRequest.AddListener(LoadingDataFromCsv);
-        gcTempLink.saveDataRequest.AddListener(SavingDataOnCsv);
+		gcTempLink.loadDataRequest.AddListener(LoadingDataFromCsv);
+		gcTempLink.saveDataRequest.AddListener(SavingDataOnCsv);
 
-    } 
-    #endregion
+	}
+	#endregion
 
-    private void LoadingDataFromCsv()
-    {
+	private void LoadingDataFromCsv()
+	{
+		string[] tempPaths = new string[];
+		ReadCsv(path + "maps.csv");
 
         // fede qui ci sei tu, i dati che la logica utilizzerà sono nella variabile GameContN.playerdataStati, salva tutto li dentro
 
@@ -25,6 +30,22 @@ public class SaveLoadManager : MonoBehaviour {
 
       
     }
+
+	private void ReadCsv(string _fileName, out List<string> readOutPut) 
+	{
+		List<string> readOutPut = new List<string>();
+		int counter = 0;
+		StreamReader sr = File.OpenText(path + _fileName);
+		string s;
+		while ((s = sr.ReadLine()) != null)
+		{
+			if (counter == 0)
+			{
+				counter++;
+				continue;
+			}
+			readOutPut.Add(sr.ReadLine());
+	} 
 
     private void SavingDataOnCsv()
     {
