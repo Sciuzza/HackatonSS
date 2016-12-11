8f55ba4bbaf20056336d9d166093951437628253
Zambon Federico <zambon.fe@gmail.com>
2016-12-11 09:52:11 +0100

    no message

--- Assets/Scripts/New Ones/SaveLoadManager.cs
+++ Assets/Scripts/New Ones/SaveLoadManager.cs
@@ -10,30 +10,114 @@ public class SaveLoadManager : MonoBehaviour
 
     #region Taking References and Linking Events
     void Awake()
-	{
-		GameContN gcTempLink = this.gameObject.GetComponent<GameContN>();
+    {
+        GameContN gcTempLink = this.gameObject.GetComponent<GameContN>();
 
-		gcTempLink.loadDataRequest.AddListener(LoadingDataFromCsv);
-		gcTempLink.saveDataRequest.AddListener(SavingDataOnCsv);
+        gcTempLink.loadDataRequest.AddListener(LoadingDataFromCsv);
+        gcTempLink.saveDataRequest.AddListener(SavingDataOnCsv);
 
-	}
+    }
     #endregion
 
     private void LoadingDataFromCsv()
     {
         path = Application.persistentDataPath;
         List<string[]> nextStepPath = new List<string[]>();
-        //string[] tempPaths = new string[];
-        ReadCsv(path + "maps.csv", out nextStepPath);
-        /*
-        sensibleMapData newCity;
-        newCity.mapName = nextStepPath[0][0];
-            GameContN.playerDatasStatic.mapData.Add(new sensibleMapData());
-        // fede qui ci sei tu, i dati che la logica utilizzerà sono nella variabile GameContN.playerdataStati, salva tutto li dentro
-        */
-        GameContN.Debugging("Data Loaded");
 
-      
+        ReadCsv(path + "saveFile.csv", out nextStepPath);
+        //load saveFile
+        sensibleGeneralData generalData = new sensibleGeneralData();
+        //generalData.lastCityVisited = nextStepPath[0][0];
+        //generalData.lastNewsVisited = nextStepPath[0][1];
+        //generalData.lastSceneVisited = nextStepPath[0][2];
+        //generalData.newsSelected = nextStepPath[0][3];
+
+        //load and create cities
+        ReadCsv(path + "maps.csv", out nextStepPath);
+
+        for (int i = 0; i < nextStepPath.Count; i++)
+        {
+            sensibleMapData newCity = new sensibleMapData();
+            newCity.newsData = new List<sensibleNewsData>();
+
+            if (i == 0)
+            {
+                newCity.mapName = cities.Milan;
+                //newCity.mapName = nextStepPath[0][0];  
+                generalData.mapData.Add(newCity);
+
+                //load and create news
+                ReadCsv(path + nextStepPath[i][0] + ".csv", out nextStepPath);
+                for (int j = 0; j < nextStepPath.Count; j++)
+                {
+                    sensibleNewsData newNews = new sensibleNewsData();
+                    newNews.scenesData = new List<sensibleSceneData>();
+                    newNews.newsName = nextStepPath[j][0];
+                    newNews.newsInfoText = nextStepPath[j][1];
+                    newNews.playerCurrentScore = int.Parse(nextStepPath[j][2]);
+                    newCity.newsData.Add(newNews);
+
+                    //load and create scenes
+                    ReadCsv(path + nextStepPath[j][0] + ".csv", out nextStepPath);
+                    for (int k = 0; k < nextStepPath.Count; k++)
+                    {
+                        sensibleSceneData newScene = new sensibleSceneData();
+                        newScene.cluesData = new List<sensibleClueData>();
+                        newScene.sceneIndex = int.Parse(nextStepPath[k][0]);
+                        newNews.scenesData.Add(newScene);
+                        //load and create clues
+                        ReadCsv(path + nextStepPath[k][0] + ".csv", out nextStepPath);
+                        for (int l = 0; l < nextStepPath.Count; l++)
+                        {
+                            sensibleClueData newClue = new sensibleClueData();
+                            newClue.clueName = nextStepPath[l][0];
+                            newClue.clueInfoText = nextStepPath[l][1];
+                            newClue.hasBeenFound = bool.Parse(nextStepPath[l][2]);
+                            newScene.cluesData.Add(newClue);
+                        }
+                    }
+                }
+            }
+            else if (i == 1)
+            {
+                newCity.mapName = cities.Rome;
+                //newCity.mapName = nextStepPath[0][0];  
+                generalData.mapData.Add(newCity);
+
+                //load and create news
+                ReadCsv(path + nextStepPath[i][0] + ".csv", out nextStepPath);
+                for (int j = 0; j < nextStepPath.Count; j++)
+                {
+                    sensibleNewsData newNews = new sensibleNewsData();
+                    newNews.scenesData = new List<sensibleSceneData>();
+                    newNews.newsName = nextStepPath[j][0];
+                    newNews.newsInfoText = nextStepPath[j][1];
+                    newNews.playerCurrentScore = int.Parse(nextStepPath[j][2]);
+                    newCity.newsData.Add(newNews);
+
+                    //load and create scenes
+                    ReadCsv(path + nextStepPath[j][0] + ".csv", out nextStepPath);
+                    for (int k = 0; k < nextStepPath.Count; k++)
+                    {
+                        sensibleSceneData newScene = new sensibleSceneData();
+                        newScene.cluesData = new List<sensibleClueData>();
+                        newScene.sceneIndex = int.Parse(nextStepPath[k][0]);
+                        newNews.scenesData.Add(newScene);
+                        //load and create clues
+                        ReadCsv(path + nextStepPath[k][0] + ".csv", out nextStepPath);
+                        for (int l = 0; l < nextStepPath.Count; l++)
+                        {
+                            sensibleClueData newClue = new sensibleClueData();
+                            newClue.clueName = nextStepPath[l][0];
+                            newClue.clueInfoText = nextStepPath[l][1];
+                            newClue.hasBeenFound = bool.Parse(nextStepPath[l][2]);
+                            newScene.cluesData.Add(newClue);
+                        }
+                    }
+                }
+            }
+        }
+        GameContN.Debugging("Data Loaded");
     }
 
     private void ReadCsv(string _fileName, out List<string[]> readOutPut)
@@ -51,14 +135,84 @@ public class SaveLoadManager : MonoBehaviour
                 continue;
             }
             readOutPut.Add(sr.ReadLine().Split(separator));
+        }
+        sr.Close();
+    }
 
+    private void WriteCsv(string _fileName, List<string[]> writeInPut)
+    {
+        char[] separator = { ',' };
+        writeInPut = new List<string[]>();
+        int counter = 0;
+        StreamWriter sw = File.AppendText(path + _fileName);
+        string s = "";
+        while (writeInPut.Count > 0)
+        {
+            if (counter == 0)
+            {
+                counter++;
+                continue;
+            }
+            for (int i = 0; i < writeInPut.Count; i++)
+            {
+                for (int j = 0; j < writeInPut[i].Length; j++)
+                {
+                    if (j < writeInPut[i].Length -1)
+                    {
+                        s += writeInPut[i][j] + ',';
+                    }
+                    else
+                    {
+                        s += writeInPut[i][j];
+                    }
+
+                }
+                sw.WriteLine(s);
+            }
         }
     }
 
     private void SavingDataOnCsv()
     {
+        GameContN gcRef = FindObjectOfType<GameContN>();
+        path = Application.persistentDataPath;
+        List<string[]> stuffToSave = new List<string[]>();
+        //save general datas
+        string[] saveFileStringArray = { gcRef.playerDatas.lastCityVisited.ToString(), gcRef.playerDatas.lastNewsVisited, gcRef.playerDatas.lastSceneVisited.ToString(), gcRef.playerDatas.newsSelected };
+        stuffToSave.Add(saveFileStringArray);
+        WriteCsv(path + "saveFile.csv", stuffToSave);
 
-        // fede qui ci sei tu, i dati che la logica utilizzerà sono nella variabile GameContN.playerdataStatic, carica tutto da li
-        GameContN.Debugging("Data Saved");
+
+        //load and create cities
+        //WriteCsv(path + "maps.csv", nextStepPath);
+
+        foreach (var cityToSave in gcRef.playerDatas.mapData)
+        {
+            string[] cityString = { cityToSave.mapName.ToString() };
+            stuffToSave.Add(cityString);
+            WriteCsv(path + "maps.csv", stuffToSave);
+            foreach (var newsToSave in cityToSave.newsData)
+            {
+                stuffToSave.Clear();
+                string[] newsString = { newsToSave.newsName, newsToSave.newsInfoText, newsToSave.playerCurrentScore.ToString()};
+                stuffToSave.Add(newsString);
+                WriteCsv(path + cityToSave.mapName, stuffToSave);
+                foreach (var sceneToSave in newsToSave.scenesData)
+                {
+                    stuffToSave.Clear();
+                    string[] sceneString = { sceneToSave.sceneIndex.ToString() };
+                    stuffToSave.Add(sceneString);
+                    WriteCsv(path + newsToSave.newsName, stuffToSave);
+                    foreach (var clueToSave in sceneToSave.cluesData)
+                    {
+                        stuffToSave.Clear();
+                        string[] clueString = { clueToSave.clueName, clueToSave.clueInfoText, clueToSave.hasBeenFound.ToString() };
+                        stuffToSave.Add(clueString);
+                        WriteCsv(path + newsToSave.newsName + sceneToSave.sceneIndex, stuffToSave);
+                    }
+                }
+            }
+        }         
+        GameContN.Debugging("Data Loaded");
     }
 }
