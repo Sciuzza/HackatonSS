e76d5455f501168c930bea62436a7b2331abc8ca
Cristiano <Juza9164@gmail.com>
2016-12-13 00:24:20 +0100

    Working on News Reading Scene

--- Assets/Scripts/New Ones/TextNewsRead.cs
+++ Assets/Scripts/New Ones/TextNewsRead.cs
@@ -5,4 +5,40 @@ using UnityEngine;
 public class TextNewsRead : MonoBehaviour {
 
     public string[] newsParagraphs;
+    public TextAsset ciccio;
+
+    public string temp;
+
+    void Awake()
+    {
+        ciccio = Resources.Load("DelittoCat") as TextAsset;
+
+        temp = ciccio.text;
+
+        for (int i = 0; i < temp.Length; i++)
+        {
+            if (i % 134 == 0)
+                AppendingPageSeparator(i);
+                
+        }
+
+        newsParagraphs = ciccio.text.Split('*');
+    }
+
+    private void AppendingPageSeparator(int i)
+    {
+        int j;
+
+        for (j = i; j < temp.Length; j++)
+        {
+            if (temp[j] != ' ')
+                continue;
+            else
+                break;
+        }
+
+        if (j < temp.Length)
+            temp.Insert(j, "*");
+
+    }
 }
